5b814acb667bc3821e71d51f44b8f5da3d1bbe1b
Anubhav Jana <Anubhav.Jana97@ibm.com>
2026-02-24 14:11:43 +0530

    try test_logaddexp

--- tests/spyre_test_binaryfuncs.py
+++ tests/spyre_test_binaryfuncs.py
@@ -58,7 +58,7 @@ class SpyreTestBase(PrivateUse1TestBase):  # type: ignore[name-defined] # noqa: 
 
         torch.float16,
         torch.float32,
-        torch.float64,
+        # torch.float64,
 
     }
 
